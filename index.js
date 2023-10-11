01fdb7d92c9f6dba24298f18f9d325ed66767ee5
julianapeace <chancecordelia@gmail.com>
2023-10-11 22:45:36 +0700

    added /today

--- index.js
+++ index.js
@@ -13,15 +13,16 @@ const token = process.env.token;
 
 const bot = new TelegramBot(token, { polling: true });
 
-async function main(resp, chatId, bot) {
-  const response = await axios.get('https://api.giphy.com/v1/gifs/random?api_key=aM1XI4UOAHmlPkSLlTPnq83HPShIeSaQ&tag=&rating=g')
-  console.log(response)
+async function getGroupId(resp, chatId, bot) {
+  const response = await axios.get('https://prod.sociallayer.im/event/group_list')
+  response.forEach(r => {
+    console.log(r.username)
+  })
   bot.sendMessage(chatId, `User is verified`);
 }
 
 // Matches /gif
 bot.onText(/\/gif/, async function onGifText(msg) {
-  // main(msg.chat.id)
   const response = await axios.get(`https://api.giphy.com/v1/gifs/random?api_key=${process.env.giphy_api_key}&tag=&rating=g`)
   console.log(response.data.data.images)
   return bot.sendAnimation(msg.chat.id, response.data.data.images.downsized.url)
@@ -57,10 +58,62 @@ bot.onText(/\/echo (.+)/, function onEchoText(msg, match) {
   bot.sendMessage(msg.chat.id, resp);
 });
 
+// Matches /admin
+bot.onText(/\/admin/, async function onAdminText(msg, match) {
+  const response = await axios.get('https://prod.sociallayer.im/event/group_list')
+
+  if (msg.chat.title) {
+    console.log(msg.chat.title)
+    const re = new RegExp(msg.chat.title);
+    console.log(re)
+  
+    console.log(response.data.groups[0]);
+
+    response.data.groups.forEach(r => {
+      console.log(re.exec(r.usernames))
+    })
+  }
+
+});
+
+// Matches /test
+bot.onText(/\/test/, async function onTestText(msg, match) {
+  channel_name = msg.chat.title
+  console.log(msg.chat)
+});
+
 // Matches /today
-bot.onText(/\/today/, function onTodayText(msg) {
-  bot.sendMessage(msg.chat.id, "Here's a list of all the events today.... enjoy! :)");
-  // TODO: get API from Jiang
+bot.onText(/\/today/, async function onTodayText(msg) {
+  // retrieve group_id
+
+  /*
+  Role of Crypto Infrastructures
+  ⏰10.10 11:00am-12:00pm
+  🔗https://event.sola.day/event/454
+
+   */
+  let today = new Date(Date.now()) //todays date
+
+  let today_start = new Date(`${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate() - 1}T17:00:00.000Z`) // This is midnight for IDC TZ
+  let today_end = today_start.valueOf()/1000 + 86399 // + 24 hours
+
+  let response = await axios.get(`${process.env.url}/event/list?group_id=${process.env.group_id}&event_order=start_time_asc&page=1&start_time_from=${today_start.valueOf()/1000}&start_time_to=${today_end.valueOf()}`)
+  let events = response.data.events
+  let formattedResponse = []
+  // formattedResponse.push(`https://event.sola.day/${username}`)
+  events.forEach((event) => {
+    let start = new Date(event.start_time)
+    let end = new Date(event.ending_time)
+
+    formattedResponse.push(event.title)
+    formattedResponse.push(`⏰ ${start.toLocaleString()}`)
+    formattedResponse.push(`⏰ ${end.toLocaleString()}`)
+    formattedResponse.push(`🔗 https://event.sola.day/event/${event.id}`)
+    formattedResponse.push("\r\n")
+  })
+
+  bot.sendMessage(msg.chat.id, formattedResponse.join("\r\n"))
+  // bot.sendMessage(msg.chat.id, "Here's a list of all the events today.... enjoy! :)");
 });
 
 // Matches /photo
